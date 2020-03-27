a4190f9a3b2e6fd8e193a42bffd0b3dba7ccc51b
mustafich <mustafichpikap@gmail.com>
2020-03-27 18:24:26 +0200

    Remove comments !

---
+++ client/build/precache-manifest.e56b0bd8ea70906acffaf57bf9dbab61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00413c2e40b60a73c482",
+    "url": "/static/css/main.a64e5502.chunk.css"
+  },
+  {
+    "revision": "00413c2e40b60a73c482",
+    "url": "/static/js/main.00413c2e.chunk.js"
+  },
+  {
+    "revision": "1c64244698afecd6db1a",
+    "url": "/static/js/1.1c642446.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9de95ad29590835b2ef5707c3b657352",
+    "url": "/index.html"
+  }
+];
