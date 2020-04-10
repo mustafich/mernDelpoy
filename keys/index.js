13f2952bd7919f97ff056899bf9b9488897f29f8
Vadim <37867683+mustafich@users.noreply.github.com>
2020-04-10 21:13:47 +0300

    Update index.js

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,4 @@
 module.exports={
 MONGODB_URI:"mongodb+srv://Vadim:svMnWqGYyaJWHAul@cluster0-0oksc.mongodb.net/infoblog",
-    BASE_URL: "mongodb+srv://Vadim:svMnWqGYyaJWHAul@cluster0-0oksc.mongodb.net/infoblog"
+    BASE_URL: "https://limitless-stream-78278.herokuapp.com/"
 }
